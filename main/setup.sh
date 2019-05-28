194d718b84dddd2c9e4011690d9fef766bb4b0db
Daniel Min <danieldmin216@gmail.com>
2019-05-27 20:07:19 -0400

    Commented setup.sh

--- main/setup.sh
+++ main/setup.sh
@@ -1,9 +1,9 @@
-# Move dotfiles to home directory
-
+# Download git, vim, and neofetch
 yes | sudo apt-get install git
 yes | sudo apt-get install vim
 yes | sudo apt-get install neofetch
 
+# Move dotfiles to home directory
 echo Copied bash aliases
 cp ./.bash_aliases ~/.bash_aliases
 
