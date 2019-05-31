d074089eea16ecea0c82a8ff21fb728b677be8d5
Daniel Min <danieldmin216@gmail.com>
2019-05-30 22:04:46 -0400

    Added file to move ranger configurations

---
+++ main/ranger_setup.sh
@@ -0,0 +1,13 @@
+# Move ranger configurations
+
+mv ./commands.py ~/.config/ranger/commands.py
+echo Moved commands.py
+
+mv ./rc.conf ~/.config/ranger/rc.conf
+echo Moved rc.conf
+
+mv ./rifle.conf ~/.config/ranger/rifle.conf
+echo Moved rifle.conf
+
+mv ./scope.sh ~/.config/ranger/scope.sh
+echo Moved scope.sh
