5d59d6ba586558d4a2f1e1395c0d1b27885c772c
Daniel Min <danieldmin216@gmail.com>
2019-08-16 14:13:32 -0400

    updated i3 config and added polybar

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-# Terminate already running bar instances
+# Terminate already running bars
 killall -q polybar
 
-# Wait until the processes have been shut down
+# Wait until bars have been terminated
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
-done
+# Launch Polybar
+polybar mainbar-i3 &
