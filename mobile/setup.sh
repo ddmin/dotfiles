21407ca52f4369d1c0929159ee48089be11570d8
billy <billy@billy.com>
2020-08-10 11:33:14 -0400

    mobile setup

---
+++ mobile/setup.sh
@@ -0,0 +1,9 @@
+# bashrc
+cp ./.bashrc ~/.bashrc
+
+# vimrc
+cp ./.vimrc ~/.vimrc
+
+# termux-ohmyzsh install script
+# https://github.com/Cabbagec/termux-ohmyzsh
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
