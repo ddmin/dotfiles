de8218b59f55595e4f9f5c4b9a81ad9dd0bf8267
billy <billy@billy.com>
2020-08-10 11:44:36 -0400

    mobile change order

--- mobile/setup.sh
+++ mobile/setup.sh
@@ -1,9 +1,9 @@
+# termux-ohmyzsh install script
+# https://github.com/Cabbagec/termux-ohmyzsh
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+
 # bashrc
 cp ./.zshrc ~/.zshrc
 
 # vimrc
 cp ./.vimrc ~/.vimrc
-
-# termux-ohmyzsh install script
-# https://github.com/Cabbagec/termux-ohmyzsh
-sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
