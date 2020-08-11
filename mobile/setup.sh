b574f0a87538ae1a314584192f13feece787a9f0
billy <billy@billy.com>
2020-08-10 20:12:39 -0400

    added pkg

--- mobile/setup.sh
+++ mobile/setup.sh
@@ -1,3 +1,11 @@
+# setup storage
+termux-setup-storage
+
+# install packages
+yes | pkg install vim
+yes | pkg install git
+yes | pkg install ranger
+
 # termux-ohmyzsh install script
 # https://github.com/Cabbagec/termux-ohmyzsh
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
