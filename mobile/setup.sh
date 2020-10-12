607b123f4ed1e677fcfa0b3e201894b20b515455
billy <billy@billy.com>
2020-10-12 14:59:48 -0400

    fzf install update

--- mobile/setup.sh
+++ mobile/setup.sh
@@ -5,14 +5,11 @@ termux-setup-storage
 yes | pkg install vim
 yes | pkg install git
 yes | pkg install ranger
+yes | pkg install fzf
 
 # termux-ohmyzsh install script
 # https://github.com/Cabbagec/termux-ohmyzsh
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
-
-# Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
 
 # zshrc
 cp ./.zshrc ~/.zshrc
