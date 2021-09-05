b9e8a8a4ae9fe859693a0bbf97795c3a171d57ea
ddmin <ddmin216@outlook.com>
2021-09-05 00:26:20 -0400

    💮 basic emacs finished

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -15,10 +15,20 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
+(setq inhibit-startup-message t)
 (setq x-select-enable-clipboard t)
 (setq evil-want-C-u-scroll t)
 
+(setq make-backup-file nil)
+(setq auto-save-default nil)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; shell
+(defvar default-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list default-shell)))
+(ad-activate 'ansi-term)
 
 ;; line numbers
 (global-display-line-numbers-mode)
@@ -52,9 +62,15 @@
 (use-package powerline-evil
   :ensure t)
 
+(use-package beacon
+  :ensure t
+  :init
+  (beacon-mode 1))
+
 ;; theme
 (load-theme 'dracula t)
 (global-hl-line-mode t)
 
 (powerline-evil-vim-theme)
 (powerline-evil-vim-color-theme)
+
