2050ae14aa4f26b467927cc65885c11df1cc56ea
ddmin <danieldmin216@gmail.com>
2021-11-20 14:13:09 -0500

    ⚔️ custom faces

--- config/emacs/emacs.d/init.el
+++ config/emacs/emacs.d/init.el
@@ -50,3 +50,17 @@
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(gruber-darker-theme beacon evil undo-tree key-chord which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#181818" :foreground "#e4e4ef" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
